62d0686c7790967f047716062ea33225d287395a
Denis Belonogov <52632384+Denis3006@users.noreply.github.com>
2024-07-15 13:33:57 +0200

    remove backerndurl from App

--- carlton_frontend/src/App.tsx
+++ carlton_frontend/src/App.tsx
@@ -4,9 +4,6 @@ import Navigation from "./components/Navigation";
 import KeysApp from "./components/KeysApp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-//let backendUrl: string = "https://localhost:5000/?";
-let backendUrl: string = "https://tiefpass.pythonanywhere.com/?";
-
 function App() {
   return (
     <Router>
